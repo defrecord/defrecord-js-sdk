c68456df219d176619d328d82095bb58d0b47566
Jason Walsh <j@wal.sh>
2015-04-21 16:19:02 -0700

    Logger baseline

---
+++ src/logger/core.cljs
@@ -0,0 +1,21 @@
+(ns ^:figwheel-always logger.core
+    (:require [dommy.core]))
+
+(enable-console-print!)
+
+(println "logger.core")
+
+;; Set up the initial logger
+(defonce app-state (atom {:url "http://wal.sh/logger"}))
+
+(def config {})
+
+(defn send []
+  (js/console.log "Logging")
+  (-> (dommy/append-child (sel1 :body)
+                          (-> (dommy/create-element :div)
+                              (dommy/set-text! "Click me!"))))
+  (-> (sel1 :script)
+      (dommy/insert-after!
+       (-> (dommy/create-element "img")
+           (dommy/set-attr! "http://wal.sh/logger")))))
