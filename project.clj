aaca029dbd0eb71d15fac8ce276676ead94e221c
Jason Walsh <j@wal.sh>
2015-05-06 17:51:43 -0400

    $ lein ancient
    - all artifacts are up-to-date.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject logger "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Birler"
+  :url "http://birler.net"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [defun "0.2.0-RC"]
-                 [figwheel "0.2.5-SNAPSHOT"]
-                 [prismatic/dommy "1.0.0"]
+                 [figwheel "0.3.1"]
+                 [prismatic/dommy "1.1.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
