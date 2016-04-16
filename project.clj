89f0fbfe1ad5dd0d0f09808ec183f3ec6b17f4a1
Jason Walsh <j@wal.sh>
2016-04-16 14:20:19 -0700

    Updating dependencies and nomenclature

--- project.clj
+++ project.clj
@@ -1,46 +1,41 @@
 (defproject logger "0.1.0-SNAPSHOT"
-  :description "Birler"
-  :url "http://birler.net"
+  :description "defrecord JavaScript SDK for site owners"
+  :url "http://defrecord.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [clj-time "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.40"]
+                 [org.clojure/core.async "0.2.374"]
+                 [clj-time "0.11.0"]
                  [clj-yaml "0.4.0"]
-                 [defun "0.2.0-RC"]
-                 [figwheel "0.3.1"]
-                 [incanter "1.5.6"]
+                 [defun "0.2.0"]
+                 [figwheel "0.5.2"]
+                 [incanter "1.9.0"]
                  [lumberjack "0.1.0"]
                  [prismatic/dommy "1.1.0"]
                  ]
-
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.5-SNAPSHOT"]]
-
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-figwheel "0.5.1"]
+            ]
   :source-paths ["src"]
-
   :clean-targets ^{:protect false} ["resources/public/js/compiled"]
-
   :cljsbuild {
     :builds [{:id "dev"
-              :source-paths ["src" "dev_src"]
-              :compiler {:output-to "resources/public/js/compiled/logger.js"
+              :source-paths ["src"]
+              :compiler {:output-to "resources/public/js/compiled/defrecord.logger.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
-                         :main logger.dev
+                         :main logger.core
                          :asset-path "js/compiled/out"
                          :source-map true
                          :source-map-timestamp true
                          :cache-analysis true }}
              {:id "min"
               :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/logger.js"
+              :compiler {:output-to "resources/public/js/compiled/defrecord.logger.min.js"
                          :main logger.core
                          :optimizations :advanced
                          :pretty-print false}}]}
-
   :figwheel {
              :http-server-root "public" ;; default and assumes "resources"
              :server-port 3449 ;; default
